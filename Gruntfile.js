eac74de48469e5909aabdb8b3bea51f0f795fe5c
Sergejus <seriog@gmail.com>
2013-12-15 20:38:15 +0200

    added main page

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: [
+          'assets/css/bootstrap.css',
+          'assets/css/bootstrap-responsive.css',
+          'assets/css/style.css',
+          'assets/css/font-awesome.css'
+        ],
+        dest: 'assets/v<%= pkg.version %>/css/<%= pkg.name %>.css'
+      }
+    },
+    cssmin: {
+      minify: {
+        expand: true,
+        src: [
+          'assets/v<%= pkg.version %>/css/*.css'
+        ],
+        ext: '.min.css'
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: 'assets/font/',
+            src: ['**'],
+            dest: 'assets/v<%= pkg.version %>/font/'
+          },
+          {
+            expand: true,
+            cwd: 'assets/img/',
+            src: ['**'],
+            dest: 'assets/v<%= pkg.version %>/img/'
+          },
+          {
+            expand: true,
+            cwd: 'assets/ico/',
+            src: ['**'],
+            dest: 'assets/v<%= pkg.version %>/ico/'
+          }
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+ 
+  grunt.registerTask('default', ['concat', 'cssmin', 'copy']);
+};
